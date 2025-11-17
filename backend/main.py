cdaedc3c89f0ee1cc5a391e840bc7f9d03fe1c66
Njire Njeri <njirenjeri@gmail.com>
2025-11-17 15:05:34 +0300

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,91 @@
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
+from config import DevConfig
+from models import Recipe
+from exts import db
+
+
+
+# initialise the app
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+db.init_app(app)
+
+api = Api(app, doc = '/docs')
+
+# model / serializer
+recipe_model=api.model(
+    "Recipe",
+    {
+        "id": fields.Integer(),
+        "title": fields.String(),
+        "description": fields.String()
+    }
+)
+
+
+@api.route('/hello')
+class HelloResource(Resource):
+    def get(self):
+        return {"message": "Hello World"}
+    
+
+
+
+
+@api.route('/recipes')
+class REcipeResource(Resource):
+    @api.marshal_list_with(recipe_model)
+    def get(self):
+        """Get all Recipes"""
+        recipes = Recipe.query.all()
+        return recipes
+        
+
+    @api.marshal_with(recipe_model)
+    def post(self):
+        """Create a new recipe"""
+
+        # access data from json
+        data = request.get_json()
+
+        new_recipe = Recipe(
+            title=data.get('title'),
+            description=data.get('description')
+        )
+
+        new_recipe.save()
+        return new_recipe, 201
+        
+
+@api.route('/recipe/<int:id>')
+class RecipebyIdResource(Resource):
+    def get(self, id):
+        """Get a recipe by Id"""
+        pass
+
+    def put(self, id):
+        """Update recipe by Id"""
+        pass
+
+    def delete(self, id):
+        """delete a recipe by Id"""
+        pass
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Recipe": Recipe
+    }
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
